ebde4e3abe98b84bde1a4d92fc367fc6c65d8c0b
Saahil <neon@saahild.com>
2024-11-10 00:48:17 -0500

    update: no

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
-    let text = std::env::args().nth(1).expect("no pattern given");
-    println!("Hello, world! {:?}", text);
+    let text = std::env::args().nth(1).expect("No text to uwuify given");
+    let endResult = text.chars()
+    .map(|x| match x { 
+        // 'll' => "www",
+        'l' => "w",
+        _ => x
+    }).collect();
+    println!("{}", endResult);
 }
